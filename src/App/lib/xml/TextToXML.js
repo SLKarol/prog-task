9e3eab539cd7b940f9226fb3870a8326e2afbc4d
Stanislav Karol <slsinitsin@gmail.com>
2018-11-17 14:20:21 +0500

    Text to XML fixed

--- src/App/lib/xml/TextToXML.js
+++ src/App/lib/xml/TextToXML.js
@@ -1,26 +1,14 @@
 function TextToXML(sourceText) {
-  let dom = null;
+  let re;
   if (window.DOMParser) {
-    try {
-      dom = new DOMParser().parseFromString(sourceText, "text/xml");
-    } catch (e) {
-      dom = null;
-    }
+    const parser = new DOMParser();
+    re = parser.parseFromString(sourceText, "text/xml");
   } // Internet Explorer
-  else if (window.ActiveXObject) {
-    try {
-      dom = new window.ActiveXObject("Microsoft.XMLDOM");
-      dom.async = false;
-      if (!dom.loadXML(sourceText)) {
-        console.error(dom.parseError.reason + dom.parseError.srcText);
-        dom = null;
-      }
-    } catch (e) {
-      dom = null;
-    }
-  } else {
-    console.error("cannot parse xml string!");
+  else {
+    re = new window.ActiveXObject("Microsoft.XMLDOM");
+    re.async = false;
+    re.loadXML(sourceText);
   }
-  return dom;
+  return re;
 }
 export default TextToXML;
